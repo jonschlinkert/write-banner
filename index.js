8edb38aa6c424cfef557b849cf5a2078db194ef7
jonschlinkert <dev@sellside.com>
2014-06-28 07:45:51 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+/*!
+ * write-banner <https://github.com/jonschlinkert/write-banner>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var write = require('write');
+var strip = require('strip-banner');
+var banner = require('add-banner');
+
+var resolve = function(filepath) {
+  return path.resolve(process.cwd(), filepath);
+};
+
+function read(filepath) {
+  return fs.readFileSync(resolve(filepath), 'utf8');
+}
+
+module.exports = function writeBanner(src, dest, options) {
+  if (typeof dest === 'object') {
+    options = dest;
+    dest = src;
+  }
+  if (!dest) {
+    options = {};
+    dest = src;
+  }
+  var str = strip(read(src));
+  write.sync(dest, banner(options) + str);
+};
